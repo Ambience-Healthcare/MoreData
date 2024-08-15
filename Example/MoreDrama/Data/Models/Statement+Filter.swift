6a833f6b5a10ad123e6b1a6fc8162162ef314511
Chris Laganiere <chrislaganiere@gmail.com>
2024-08-15 00:44:46 -0700

    much better demo app

--- Example/MoreDrama/Data/Models/Statement+Filter.swift
+++ Example/MoreDrama/Data/Models/Statement+Filter.swift
@@ -1,27 +1,46 @@
 import CoreData
 import MoreData
+import SwiftUI
 
 /// Filters which specify particular entities to fetch
 public enum StatementFilter: Filtering {
 
     /// Look up subtext
-    case fuzzySearch(String)
+    case contains(String)
 
     /// relation to particular individual
     case toldBy(Person)
     case toldTo(Person)
 
+    /// compound predicate
+    case all([StatementFilter])
+    case noElements
+
     public var predicate: NSPredicate? {
         switch self {
 
-        case .fuzzySearch(let query):
+        case .contains(let query):
             return NSPredicate(format: "%K CONTAINS[cd] %@", #keyPath(Statement.content), query)
 
         case .toldBy(let person):
             return NSPredicate(format: "%K == %@", #keyPath(Statement.by), person)
 
         case .toldTo(let person):
-            return NSPredicate(format: "%@ IN %K", #keyPath(Statement.to), person)
+            return NSPredicate(format: "%@ IN %K", person, #keyPath(Statement.to))
+
+        case .all(let filters):
+            return NSCompoundPredicate(andPredicateWithSubpredicates: filters.compactMap(\.predicate))
+
+        case .noElements:
+            return NSPredicate(format: "%K == %@", #keyPath(Statement.statementID), "bogus")
         }
+    }
+
+    /// Helper for cases where you don't have person records indexed
+    static func toldBy(_ personID: String, in people: FetchedResults<Person>) -> StatementFilter {
+        guard let person = people.first(where: { $0.personID == personID }) else {
+            return .noElements
+        }
+        return .toldBy(person)
     }
 }
